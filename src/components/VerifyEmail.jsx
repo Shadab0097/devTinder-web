9fe64e9b164963dd31d4bdef7644d8078ce8d99c
shadab khan <shadabkhan2910@gmail.com>
2025-01-08 20:29:15 +0530

    mssg

--- src/components/VerifyEmail.jsx
+++ src/components/VerifyEmail.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { BASE_URL } from '../utils/constant';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react'
+// import { BASE_URL } from '../utils/constant';
+// import axios from 'axios';
+// import { useNavigate, useParams } from 'react-router-dom';
 
 
-const VerifyEmail = () => {
-    let timer
-    const navigate = useNavigate()
-    const { token } = useParams()
-    //   const [error, setError] = useState()
+// const VerifyEmail = () => {
+//     let timer
+//     const navigate = useNavigate()
+//     const { token } = useParams()
+//     //   const [error, setError] = useState()
 
-    const verifyEmail = async () => {
-        try {
-            const response = await axios.get(BASE_URL + `verify-email/${token}`, {}, { withCredentials: true });
-            // setMessage('Email verified successfully!');
-
-
-
-        } catch (err) {
-            // setMessage('Verification failed. Please try again.');
-            timer = setTimeout(() => {
-                navigate('/login')
-            }, 3000)
-            // setError('')
-
-            // setRedirecting(false)
-
-            console.error(err);
-        }
-    };
-    useEffect(() => {
+//     const verifyEmail = async () => {
+//         try {
+//             const response = await axios.get(BASE_URL + `verify-email/${token}`, {}, { withCredentials: true });
+//             // setMessage('Email verified successfully!');
 
 
-        verifyEmail();
 
-        return () => {
-            // clearTimeout(toastTimerRef.current)
-            clearTimeout(timer)
+//         } catch (err) {
+//             // setMessage('Verification failed. Please try again.');
+//             timer = setTimeout(() => {
+//                 navigate('/login')
+//             }, 3000)
+//             // setError('')
 
-        }
-    }, [token])
-    return (
-        <div>
-            <p className='text-2xl items-center font-bold text-white'>Email verified Succesfully, redirecting to login page.....</p>
-        </div>
-    )
-}
+//             // setRedirecting(false)
 
-export default VerifyEmail
+//             console.error(err);
+//         }
+//     };
+//     useEffect(() => {
+
+
+//         verifyEmail();
+
+//         return () => {
+//             // clearTimeout(toastTimerRef.current)
+//             clearTimeout(timer)
+
+//         }
+//     }, [token])
+//     return (
+//         <div>
+//             <p className='text-2xl items-center font-bold text-white'>Email verified Succesfully, redirecting to login page.....</p>
+//         </div>
+//     )
+// }
+
+// export default VerifyEmail
