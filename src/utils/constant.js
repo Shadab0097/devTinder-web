46dd62aa211f6edb61526f6c9230f247708c90e0
shadab khan <shadabkhan2910@gmail.com>
2025-01-11 12:53:47 +0530

    made url dynamic

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
 
 
-export const BASE_URL = "/api/"
-// export const BASE_URL = "http://localhost:2000/"
+// export const BASE_URL = "/api/"
+export const BASE_URL = location.hostname === 'localhost' ? "http://localhost:2000/" : "/api"
+
